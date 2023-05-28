2961e458c0e06e827a4dea1eb7857ad341da78d6
Christopher Abetianbe <christopherafum@gmail.com>
2023-05-28 13:20:48 +0000

    signup, signin,confirmation and recovery pages

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -146,7 +146,7 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 #@xray_recorder.capture('activities_home')
 def data_home():
-  data = HomeActivities.run
+  data = HomeActivities.run()
   # data = HomeActivities.run(logger=LOGGER)
   return data, 200
 
