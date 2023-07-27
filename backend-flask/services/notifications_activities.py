eb54198171ed05e2fa6cddd94a0b0a93988d451c
Christopher Abetianbe <christopherafum@gmail.com>
2023-07-27 05:02:54 +0000

    implement user edit page

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -4,7 +4,7 @@ class NotificationsActivities:
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Donny',
+      'handle':  'Donny Darko',
       'message': 'Test with the notifications backend endpoint!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
